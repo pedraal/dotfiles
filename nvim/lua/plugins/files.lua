c7c719e38db89c0a84afd5d23a3b05bd07402682
pedraal <pro@pedraal.fr>
2024-11-02 19:01:25 +0100

    feat(nvim): keymap change

--- nvim/lua/plugins/files.lua
+++ nvim/lua/plugins/files.lua
@@ -31,7 +31,7 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>p", builtin.find_files, {})
+			vim.keymap.set("n", "<leader> ", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>F", builtin.live_grep, {})
 		end,
 	},
