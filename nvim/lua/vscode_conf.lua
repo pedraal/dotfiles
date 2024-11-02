1d4e5e2ffa948088b1055c582a17aa40649620a7
pedraal <pro@pedraal.fr>
2024-11-02 19:01:12 +0100

    feat(vscode): update nvim and global config

--- nvim/lua/vscode_conf.lua
+++ nvim/lua/vscode_conf.lua
@@ -14,24 +14,16 @@ keymap("v", ">", ">gv", opts)
 
 keymap("n", "<Esc>", "<Esc>:noh<CR>", opts)
 
--- call vscode commands from neovim
-
--- general keymaps
 keymap({ "n", "v" }, "<leader>cn", "<cmd>lua require('vscode').action('notifications.clearAll')<CR>")
-keymap({ "n", "v" }, "<leader>t", "<cmd>lua require('vscode').action('workbench.action.terminal.toggleTerminal')<CR>")
-keymap({ "n", "v" }, "<leader>ca", "<cmd>lua require('vscode').action('editor.action.quickFix')<CR>")
+-- keymap({ "n", "v" }, "<leader>t", "<cmd>lua require('vscode').action('workbench.action.terminal.toggleTerminal')<CR>")
+keymap({ "n", "v" }, "<leader>ga", "<cmd>lua require('vscode').action('editor.action.quickFix')<CR>")
 keymap({ "n", "v" }, "<leader>gf", "<cmd>lua require('vscode').action('editor.action.formatDocument')<CR>")
--- keymap({ "n", "v" }, "<leader>gh", "<cmd>lua require('vscode').action('editor.action.showHover')<CR>")
 -- keymap({ "n", "v" }, "<leader>gi", "<cmd>lua require('vscode').action('editor.action.goToImplementation')<CR>")
 -- keymap({ "n", "v" }, "<leader>gt", "<cmd>lua require('vscode').action('editor.action.goToTypeDefinition')<CR>")
--- keymap({ "n", "v" }, "<leader>gd", "<cmd>lua require('vscode').action('editor.action.revealDefinition')<CR>")
-keymap({ "n", "v" }, "<leader>b", "<cmd>lua require('vscode').action('workbench.view.explorer')<CR>")
-keymap({ "n", "v" }, "<leader> ",
-	"<cmd>lua require('vscode').action('workbench.action.quickOpen')<CR>")
-keymap({ "n", "v" }, "<leader>p", "<cmd>lua require('vscode').action('workbench.action.showCommands')<CR>")
+-- keymap({ "n", "v" }, "<leader>b", "<cmd>lua require('vscode').action('workbench.view.explorer')<CR>")
+keymap({ "n", "v" }, "<leader> ", "<cmd>lua require('vscode').action('workbench.action.quickOpen')<CR>")
+keymap({ "n", "v" }, "<leader>P", "<cmd>lua require('vscode').action('workbench.action.showCommands')<CR>")
 
-
--- harpoon keymaps
 keymap({ "n", "v" }, "<leader>ha", "<cmd>lua require('vscode').action('vscode-harpoon.addEditor')<CR>")
 keymap({ "n", "v" }, "<leader>ho", "<cmd>lua require('vscode').action('vscode-harpoon.editorQuickPick')<CR>")
-keymap({ "n", "v" }, "<leader>he", "<cmd>lua require('vscode').action(c'vscode-harpoon.editEditors')<CR>")
+keymap({ "n", "v" }, "<leader>he", "<cmd>lua require('vscode').action('vscode-harpoon.editEditors')<CR>")
