89c05733d571dd8f7b9dfb971bc6592558e5446e
pedraal <pro@pedraal.fr>
2025-08-15 02:23:35 +0200

    feat(nvim): add dbui

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -258,5 +258,22 @@ return {
       })
       require("image").enable()
     end
+  },
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod',                     lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
   }
 }
