1f8667cf7af78ab35591962d2ebc091b0d7882a6
pedraal <pro@pedraal.fr>
2025-09-01 09:30:58 +0200

    feat(nvim): opencode bindings

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -7,4 +7,19 @@ return {
       })
     end,
   },
+  {
+    'NickvanDyke/opencode.nvim',
+    ---@type opencode.Opts
+    opts = {
+      -- Your configuration, if any — see lua/opencode/config.lua
+    },
+    keys = {
+      { '<leader>oA', function() require('opencode').ask() end,                  desc = 'Ask opencode', },
+      { '<leader>oa', function() require('opencode').ask('@cursor: ') end,       desc = 'Ask opencode about this',      mode = 'n', },
+      { '<leader>oa', function() require('opencode').ask('@selection: ') end,    desc = 'Ask opencode about selection', mode = 'v', },
+      { '<leader>ot', function() require('opencode').toggle() end,               desc = 'Toggle embedded opencode', },
+      { '<leader>on', function() require('opencode').command('session_new') end, desc = 'New session', },
+      { '<leader>op', function() require('opencode').select_prompt() end,        desc = 'Select prompt',                mode = { 'n', 'v', }, },
+    },
+  }
 }
