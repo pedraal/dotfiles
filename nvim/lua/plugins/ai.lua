3e0171636c251a0647a6b60a5f68c070644aa7b7
pedraal <pro@pedraal.fr>
2026-01-15 23:11:16 +0100

    nvim: clean ai config

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -2,24 +2,13 @@ local is_light_config = os.getenv("NVIM_LIGHT_CONFIG") ~= nil
 
 return {
   {
-    "supermaven-inc/supermaven-nvim",
-    cond = not is_light_config,
-    config = function()
-      require("supermaven-nvim").setup({
-        ignore_filetypes = { "neo-tree", "neo-tree-popup" }
-      })
-    end,
-  },
-  {
     'NickvanDyke/opencode.nvim',
     cond = not is_light_config,
     config = function()
       vim.g.opencode_opts = {
         provider = {
           enabled = "tmux",
-          tmux = {
-            -- ...
-          }
+          tmux = {}
         }
       }
 
@@ -33,10 +22,6 @@ return {
         { desc = "Add to opencode" })
       vim.keymap.set({ "n", "t" }, "<leader>ot", function() require("opencode").toggle() end,
         { desc = "Toggle opencode" })
-      -- vim.keymap.set("n", "<C-U>", function() require("opencode").command("session.half.page.up") end,
-      --   { desc = "opencode half page up" })
-      -- vim.keymap.set("n", "<C-D>", function() require("opencode").command("session.half.page.down") end,
-      --   { desc = "opencode half page down" })
     end,
   }
 }
