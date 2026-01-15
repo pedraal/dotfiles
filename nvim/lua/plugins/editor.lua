05d5349456d8d3ee4dc873ea80f7abbdbaa90902
pedraal <pro@pedraal.fr>
2026-01-15 23:11:25 +0100

    nvim: add resize plugin

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -75,4 +75,14 @@ return {
       })
     end,
   },
+  {
+    '0xm4n/resize.nvim',
+    version = '*',
+    config = function()
+      vim.keymap.set("n", "<C-Left>", "<cmd>lua require('resize').ResizeLeft()<CR>", { silent = true })
+      vim.keymap.set("n", "<C-Right>", "<cmd>lua require('resize').ResizeRight()<CR>", { silent = true })
+      vim.keymap.set("n", "<C-Up>", "<cmd>lua require('resize').ResizeUp()<CR>", { silent = true })
+      vim.keymap.set("n", "<C-Down>", "<cmd>lua require('resize').ResizeDown()<CR>", { silent = true })
+    end,
+  },
 }
